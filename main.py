2fde7687e5aeed6e0e9d7e4ddaacd1655dfaf79e
Andrii Liubov <andrii.liubov@gmail.com>
2023-12-18 14:22:06 +0200

    Homework6 fixed

--- main.py
+++ main.py
@@ -40,30 +40,46 @@ def remove_empty_folders(path):
             except OSError:
                 pass
 
+def remove_unarchived_file(path):
+    for item in path.iterdir():
+        if item.is_file():
+            try:
+                item.unlink()
+            except OSError:
+                pass
+
 def main(folder_path):
     print(folder_path)
     scan.scan(folder_path)
 
-    for file in scan.images:
-        handle_file(file, folder_path, "images")
+    for key, value in scan.registered_extensions.items():
+        for file in value:
+            if key == "archives":
+                handle_archive(file, folder_path, "archives")
+            else:
+                handle_file(file, folder_path, key)
 
-    for file in scan.video:
-        handle_file(file, folder_path, "video")
+    # for file in scan.images:
+    #     handle_file(file, folder_path, "images")
 
-    for file in scan.documents:
-        handle_file(file, folder_path, "documents")
+    # for file in scan.video:
+    #     handle_file(file, folder_path, "video")
 
-    for file in scan.audio:
-        handle_file(file, folder_path, "audio")
+    # for file in scan.documents:
+    #     handle_file(file, folder_path, "documents")
 
-    for file in scan.archives:
-        handle_archive(file, folder_path, "archives")
+    # for file in scan.audio:
+    #     handle_file(file, folder_path, "audio")
 
-    for file in scan.others:
-        handle_file(file, folder_path, "others")
+    # for file in scan.archives:
+    #     handle_archive(file, folder_path, "archives")
+
+    # for file in scan.others:
+    #     handle_file(file, folder_path, "others")
 
     
     remove_empty_folders(folder_path)
+    remove_unarchived_file(folder_path)
 
 if __name__ == '__main__':
     path = sys.argv[1]
@@ -71,3 +87,16 @@ if __name__ == '__main__':
 
     folder = Path(path)
     main(folder.resolve())
+
+
+    print(f"images: {scan.images}")
+    print(f"video: {scan.video}")
+    print(f"documents: {scan.documents}")
+    print(f"audio: {scan.audio}")
+    print(f"archives: {scan.archives}")
+    print(f"others: {scan.others}")
+    
+    print(f"All extensions: {scan.extensions}")
+    print(f"Unknown extensions: {scan.unknown}")
+
+ 
