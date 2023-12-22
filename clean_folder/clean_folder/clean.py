dd5f19ea013be5b3ca4ef5054130b3ce8c734cd5
Andrii Liubov <andrii.liubov@gmail.com>
2023-12-22 11:55:21 +0200

    Homework7

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -1,6 +1,14 @@
+import re
 import sys
+import shutil
 from pathlib import Path
 
+
+UKRAINIAN_SYMBOLS = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "ju", "ja")
+
+TRANS = {}
 
 images = list()   #('JPEG', 'PNG', 'JPG', 'SVG');
 video = list()    #('AVI', 'MP4', 'MOV', 'MKV');
@@ -20,6 +28,17 @@ registered_extensions = {
     'archives': archives,
     'others': others
 }
+
+
+for key, value in zip(UKRAINIAN_SYMBOLS, TRANSLATION):
+    TRANS[ord(key)] = value
+    TRANS[ord(key.upper())] = value.upper()
+
+def normalize(name: str) -> str:
+    name, *extension = name.split('.')
+    new_name = name.translate(TRANS)
+    new_name = re.sub(r'\W', '_', new_name)
+    return f"{new_name}.{'.'.join(extension)}"
 
 def get_extensions(file_name):
     return Path(file_name).suffix[1:].upper()
@@ -66,21 +85,62 @@ def scan(folder):
                 unknown.add(extension)
                 others.append(new_name)
 
-if __name__ == '__main__':
-    path = sys.argv[1]
 
-    folder = Path(path)
-    
-    scan(folder)
-    
-    
-    print(f"images: {images}")
-    print(f"video: {video}")
-    print(f"documents: {documents}")
-    print(f"audio: {audio}")
-    print(f"archives: {archives}")
-    print(f"others: {others}")
-    
-    print(f"All extensions: {extensions}")
-    print(f"Unknown extensions: {unknown}")
-    
+def handle_file(path, root_folder, dist):
+    target_folder = root_folder/dist
+    target_folder.mkdir(exist_ok=True)
+    path.replace(target_folder/normalize(path.name))
+
+def handle_archive(path, root_folder, dist):
+    target_folder = root_folder / dist
+    target_folder.mkdir(exist_ok=True)
+
+    new_name = normalize(path.name)
+    new_name = re.sub(r'.zip|.gz|.tar', '', new_name)
+
+    archive_folder = target_folder / new_name
+    archive_folder.mkdir(exist_ok=True)
+
+    try:
+        shutil.unpack_archive(str(path.resolve()), str(archive_folder.resolve()))
+    except shutil.ReadError:
+        archive_folder.rmdir()
+    except FileNotFoundError:
+        archive_folder.rmdir()
+    path.unlink()
+
+
+def remove_empty_folders(path):
+    for item in path.iterdir():
+        if item.is_dir():
+            remove_empty_folders(item)
+            try:
+                item.rmdir()
+            except OSError:
+                pass
+
+def remove_unarchived_file(path):
+    for item in path.iterdir():
+        if item.is_file():
+            try:
+                item.unlink()
+            except OSError:
+                pass
+
+
+def main():
+    folder_path = Path(sys.argv[1])
+    scan(folder_path)
+
+    for key, value in registered_extensions.items():
+        for file in value:
+            if key == "archives":
+                handle_archive(file, folder_path, "archives")
+            else:
+                handle_file(file, folder_path, key)
+
+    remove_empty_folders(folder_path)
+    remove_unarchived_file(folder_path)
+
+if __name__ == '__main__':
+    main()
